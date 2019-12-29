57d3be77f596fd2d0e69cb976a9c3026f4abd40b
Dima Chechetkin <dima.chechetkin@appian.com>
2019-12-28 21:27:09 -0500

    --

--- src/main/java/com/onkiup/linker/parser/Rule.java
+++ src/main/java/com/onkiup/linker/parser/Rule.java
@@ -25,15 +25,18 @@ import com.onkiup.linker.util.TypeUtils;
 // in 0.2.2:
 // - added "C" type parameter
 // - made it implement Consumer
+
 /**
  * Main interface for all grammar definitions
  */
 public interface Rule {
 
   class Metadata {
-    private static Map<Rule, PartialToken> metadata = Collections.synchronizedMap(new WeakHashMap<>());
-    private static Map<Rule, Map<Class<? extends Extension>, Extension<?>>> extensions = Collections.synchronizedMap(new WeakHashMap<>());
-    private static Map<Class<? extends Rule>, Map<Class<?>, Conversion<?, ?>>> conversions = Collections.synchronizedMap(new HashMap<>());
+    private static Map<Rule,PartialToken> metadata = Collections.synchronizedMap(new WeakHashMap<>());
+    private static Map<Rule,Map<Class,Extension<?>>> extensions = Collections.synchronizedMap(
+        new WeakHashMap<>());
+    private static Map<Class<? extends Rule>,Map<Class<?>,Conversion<?,?>>> conversions = Collections.synchronizedMap(
+        new HashMap<>());
 
     public static Optional<PartialToken<?>> metadata(Rule rule) {
       return Optional.ofNullable(metadata.get(rule));
@@ -66,23 +69,19 @@ public interface Rule {
    * @return parent token or null if this token is root token
    */
   default <R extends Rule> Optional<R> parent() {
-    return Metadata.metadata(this)
-      .map(meta -> {
-        do {
-          meta = (PartialToken) meta.parent().orElse(null);
-        } while (!(meta instanceof RuleToken));
-        return (CompoundToken) meta;
-      })
-      .flatMap(PartialToken::token);
+    return Metadata.metadata(this).map(meta -> {
+      do {
+        meta = (PartialToken)meta.parent().orElse(null);
+      } while (!(meta instanceof RuleToken));
+      return (CompoundToken)meta;
+    }).flatMap(PartialToken::token);
   }
 
   /**
    * @return true if this token was successfully populated; false if parser is still working on some of the token's fields
    */
   default boolean populated() {
-    return Metadata.metadata(this)
-      .map(PartialToken::isPopulated)
-      .orElse(false);
+    return Metadata.metadata(this).map(PartialToken::isPopulated).orElse(false);
   }
 
   default void onPopulated() {
@@ -102,7 +101,7 @@ public interface Rule {
    * Called by parser every time it updates the token
    */
   default void reevaluate() {
-    
+
   }
 
   /**
@@ -110,7 +109,7 @@ public interface Rule {
    * called by arser every time it detaches the token from the tree
    */
   default void invalidate() {
-  
+
   }
 
   default CharSequence source() {
@@ -130,52 +129,65 @@ public interface Rule {
    * Creates or returns previously created extension object associated with this Rule
    *
    * @param type the type of extension object to create or return
-   * @param <E> the type of extension object
+   * @param <E>  the type of extension object
    * @return extension object
    */
-  default <T extends Rule, E extends Extension<T>> E as(Class<E> type) {
+  default <T extends Rule, E> E as(Class<E> type) {
     synchronized (this) {
+
+      if (type.isInstance(this)) {
+        return (E)this;
+      }
+
       if (!Metadata.extensions.containsKey(this)) {
         Metadata.extensions.put(this, Collections.synchronizedMap(new HashMap<>()));
       }
 
-      Map<Class<? extends Extension>, Extension<?>> extensions = Metadata.extensions.get(this);
+      Map<Class,Extension<?>> extensions = Metadata.extensions.get(this);
       if (!extensions.containsKey(type)) {
         try {
-         Class<? extends E>[] candidates = (Class<? extends E>[])ParserContext.get().subClasses(type)
-              .filter(TokenGrammar::isConcrete)
-              .filter(Extension.class::isAssignableFrom)
-              .filter(impl -> {
-                Class<?> extended = TypeUtils.typeParameter(impl, Extension.class, 0);
-                return extended.isAssignableFrom(getClass());
-              })
-              .toArray();
+          Class<E>[] candidates;
+
+          if (!TypeUtils.isConcrete(type)) {
+            candidates = ParserContext.get()
+                .subClasses(type)
+                .filter(TokenGrammar::isConcrete)
+                .filter(Extension.class::isAssignableFrom)
+                .filter(impl -> {
+                  Class<?> extended = TypeUtils.typeParameter((Class<? extends Extension>) impl, Extension.class, 0);
+                  return extended.isAssignableFrom(getClass());
+                })
+                .toArray(Class[]::new);
+          } else {
+            candidates = new Class[] {type};
+          }
 
           if (candidates.length == 1) {
             if (extensions.containsKey(candidates[0])) {
               extensions.put(type, extensions.get(candidates[0]));
             } else {
-              E extension = null;
+              Extension<T> extension = null;
               if (getClass().isEnum() && candidates[0].isEnum()) {
                 // creating extension enum with same ordinal as base enum
                 int ordinal = ((Enum)this).ordinal();
-                extension = candidates[0].getEnumConstants()[ordinal];
+                extension = (Extension<T>)candidates[0].getEnumConstants()[ordinal];
               } else {
-                extension = candidates[0].newInstance();
+                extension = (Extension<T>)candidates[0].newInstance();
               }
               extensions.put(type, extension);
               extensions.put(candidates[0], extension);
               Extension.Metadata.base(extension, (T)this);
             }
-          } else if (candidates.length > 1){
-            throw new IllegalStateException("Too many extension candidates: \n" +
-                Arrays.stream(candidates).map(Class::getName).collect(Collectors.joining("\n\t", "\t", "\n"))
-            );
+          } else if (candidates.length > 1) {
+            throw new IllegalStateException("Too many extension candidates: \n" + Arrays.stream(candidates)
+                .map(Class::getName)
+                .collect(Collectors.joining("\n\t", "\t", "\n")));
           } else {
             throw new ClassNotFoundException("Failed to find extension class for '" + type.getName() + "'");
           }
         } catch (Exception e) {
-          throw new RuntimeException("Failed to extend '" + getClass().getName() + "' into '" + type.getName() + "'", e);
+          throw new RuntimeException(
+              "Failed to extend '" + getClass().getName() + "' into '" + type.getName() + "'", e);
         }
       }
       return (E)extensions.get(type);
@@ -185,44 +197,52 @@ public interface Rule {
   default <E> E to(Class<E> target) {
     synchronized (this) {
       try {
-        if(!Metadata.conversions.containsKey(getClass())) {
+        if (!Metadata.conversions.containsKey(getClass())) {
           Metadata.conversions.put(getClass(), Collections.synchronizedMap(new HashMap<>()));
         }
 
-        Map<Class<?>, Conversion<?, ?>> myConversions = Metadata.conversions.get(getClass());
+        Map<Class<?>,Conversion<?,?>> myConversions = Metadata.conversions.get(getClass());
 
         if (!myConversions.containsKey(target)) {
-          Class<Conversion<?, ?>>[] candidates = (Class<Conversion<?, ?>>[]) ParserContext.get().subClasses(Conversion.class)
-            .filter(TokenGrammar::isConcrete)
-            .filter(impl -> {
-              Class<?> from = TypeUtils.typeParameter(impl, Conversion.class, 0);
-              Class<?> to = TypeUtils.typeParameter(impl, Conversion.class, 1);
-              return from.isAssignableFrom(getClass()) && target.isAssignableFrom(to);
-            })
-            .toArray();
+          Class<Conversion<?,?>>[] candidates = (Class<Conversion<?,?>>[])ParserContext.get()
+              .subClasses(Conversion.class)
+              .filter(TokenGrammar::isConcrete)
+              .filter(impl -> {
+                Class<?> from = TypeUtils.typeParameter(impl, Conversion.class, 0);
+                Class<?> to = TypeUtils.typeParameter(impl, Conversion.class, 1);
+                return from.isAssignableFrom(getClass()) && target.isAssignableFrom(to);
+              })
+              .toArray();
 
-          Class<? extends Conversion<?, ?>> converterClass = null;
+          Class<? extends Conversion<?,?>> converterClass = null;
           if (candidates.length == 1) {
             converterClass = candidates[0];
           } else if (candidates.length > 1) {
-            throw new IllegalStateException("Too many converters from '" + getClass().getCanonicalName() + "' to class '" + target.getCanonicalName() + "':\n" + 
-              Arrays.stream(candidates).map(Class::getName).collect(Collectors.joining("\n\t", "\t", "\n"))
-            );
+            throw new IllegalStateException(
+                "Too many converters from '" + getClass().getCanonicalName() + "' to class '" +
+                    target.getCanonicalName() + "':\n" + Arrays.stream(candidates)
+                    .map(Class::getName)
+                    .collect(Collectors.joining("\n\t", "\t", "\n")));
           } else {
-            throw new ClassNotFoundException("Failed to find converter from '" + getClass().getCanonicalName() + "' to class '" + target.getCanonicalName() + "'");
+            throw new ClassNotFoundException(
+                "Failed to find converter from '" + getClass().getCanonicalName() + "' to class '" +
+                    target.getCanonicalName() + "'");
           }
 
           try {
             myConversions.put(target, converterClass.newInstance());
           } catch (Exception e) {
-            throw new RuntimeException("Failed to instantiate converter '" + converterClass.getCanonicalName() + "'", e);
+            throw new RuntimeException(
+                "Failed to instantiate converter '" + converterClass.getCanonicalName() + "'", e);
           }
         }
 
         Conversion converter = myConversions.get(target);
-        return (E) converter.convert(this);
+        return (E)converter.convert(this);
       } catch (Exception e) {
-        throw new RuntimeException(String.format("Failed to convert '%s' to '%s'", getClass().getCanonicalName(), target.getCanonicalName()), e);
+        throw new RuntimeException(
+            String.format("Failed to convert '%s' to '%s'", getClass().getCanonicalName(),
+                target.getCanonicalName()), e);
       }
     }
   }
